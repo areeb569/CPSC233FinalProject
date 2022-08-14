d62b7dd3fe1dee8bf6eb37545411de38f540dc74
mdareeb.bari1 <mdareeb.bari1@ucalgary.ca>
2022-08-13 19:28:33 -0600

    Added a title to the intro page and 4 buttons: the first button asks the user to enter the account information, the second button is to deposit money, the third button is for withdrawal, and the fourth button shows the user's account information if they have entered information from the beginning.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,22 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/IntroScene.fxml"));
+			Scene scene = new Scene(root,700,500);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Bank Management System");
 			primaryStage.show();
