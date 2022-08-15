2cc36164a83804e69b4532c4928532c2a0eae7c5
ruth.ngolaminga <ruth.ngolaminga@IC-CS233-22.uc.ucalgary.ca>
2022-08-15 02:40:04 -0400

    Created a class called, bank account. which contains the deposit and the withdraw method suggested by the user enterface. It will also contain the customer's name and id which will be implemented latter

---
+++ src/application/BankAccount.java
@@ -0,0 +1,27 @@
+package application;                            
+ public class BankAccount {                     
+	double balance;                                
+	//Investment InvestAccount;                      
+	//Savings savingsAccount;                        
+	//Checking checkingAccount;                      
+	public static String customerName;             
+	public static String customerId;               
+	                                               
+	BankAccount(String name, String id)            
+	 {                                             
+		 customerName = name;                         
+		 customerId = id;                             
+	 }                                             
+	                                               
+	//public abstract void deposit(double amount); 
+	                                               
+	                                               
+	//public abstract void withdraw(double amount);
+	                                               
+	                                               
+	                                               
+	                                               
+                                                
+}                                               
+                                                
+                                                
